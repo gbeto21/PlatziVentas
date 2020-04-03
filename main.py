d23a85de9bfbab35bfbb43ce711aa1e8bc36de9b
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-02 18:51:20 -0600

    The function of capture a new client with a validation was added to the program.

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ clients = 'pablo,ricardo,'
 
 def create_client(client_name):
     global clients
-    clients += client_name
-    _add_comma()
+
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        print('Client already is in the client\'s list')
 
 
 def list_clients():
@@ -18,7 +22,25 @@ def _add_comma():
     clients += ','
 
 
+def _print_welcome():
+    print('WELCOME TO PLATZI VENTAS')
+    print('*' * 20)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[D]elete client')
+
+
 if __name__ == "__main__":
-    list_clients()
-    create_client('David')
+    _print_welcome()
+
+    capture = input()
+    if capture == 'C':
+        client_name = input('What is the client name? ')
+        create_client(client_name)
+        list_clients()
+    elif capture == 'D':
+        pass
+    else:
+        print('Invalid command')
+
     list_clients()
