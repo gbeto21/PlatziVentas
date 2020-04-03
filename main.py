9f28ef56d91df6b1f302a12785949d7d2fb3714f
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-02 19:24:06 -0600

    The update function was created.

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ def create_client(client_name):
         print('Client already is in the client\'s list')
 
 
+def update_client(client_name,updated_client_name):
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', updated_client_name + ',')
+    else:
+        print('Client is not in clients list')
+        
+
 def list_clients():
     global clients
     print(clients)
@@ -27,19 +35,30 @@ def _print_welcome():
     print('*' * 20)
     print('What would you like to do today?')
     print('[C]reate client')
+    print('[U]pdate client')
     print('[D]elete client')
+
+
+def _get_client_name():
+    return input('What is the client name? ')
 
 
 if __name__ == "__main__":
     _print_welcome()
 
-    capture = input()
-    if capture == 'C':
-        client_name = input('What is the client name? ')
+    command = input()
+    command = command.upper()
+
+    if command == 'C':
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
-    elif capture == 'D':
+    elif command == 'D':
         pass
+    elif command == 'U':
+        client_name =  _get_client_name()
+        updated_client_name = input('What is the updated client name? ')
+        update_client(client_name, updated_client_name)
     else:
         print('Invalid command')
 
